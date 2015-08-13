a6e3f0a4914050df331eb5a8033ab85603c525ef
alex <alex@localhost.localdomain>
2015-08-13 15:45:07 +0300

    one more time..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snami::Application.config.secret_key_base = 'e8b8cd16be423aa3b726bbb86ef5bd2bff6a1afd3f3e3a7481185870f07724d6f287b03a87c7d9adc2f8aa97036b33749b0b8fb7ad094787a4051b0db27683d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snami::Application.config.session_store :cookie_store, key: '_snami_session'
